5094bf0c5f852bfa5eeaa20432e43266febe3310
Hossein L. Raei <hrdip.2018@gmail.com>
2024-04-21 22:17:24 +0800

    paginator

--- core/shop/views.py
+++ core/shop/views.py
@@ -11,7 +11,7 @@ from .models import ProductModel, ProductStatusType
 class ShopProductGridView(ListView):
     template_name = 'shop/product-grid.html'
     queryset = ProductModel.objects.filter(status=ProductStatusType.publish.value)
-    
+    paginate_by = 9
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["total_products"] = self.get_queryset().count()
