7f4c08767bddcf1520f31c03b3cf2f4011465669
Hossein L. Raei <hrdip.2018@gmail.com>
2024-04-22 20:43:30 +0800

    filtering by price and category and sorting

--- core/shop/views.py
+++ core/shop/views.py
@@ -5,16 +5,36 @@ from django.views.generic import (
     ListView,
     DetailView,
 )
-from .models import ProductModel, ProductStatusType
+from .models import ProductModel, ProductStatusType, ProductCategoryModel
+from django.core.exceptions import FieldError
 # Create your views here.
 
 class ShopProductGridView(ListView):
     template_name = 'shop/product-grid.html'
-    queryset = ProductModel.objects.filter(status=ProductStatusType.publish.value)
     paginate_by = 9
+
+    def get_queryset(self):
+        queryset = ProductModel.objects.filter(
+            status=ProductStatusType.publish.value)
+        if search_q := self.request.GET.get("q"):
+            queryset = queryset.filter(title__icontains=search_q)
+        if category_id := self.request.GET.get("category_id"):
+            queryset = queryset.filter(category__id=category_id)
+        if min_price := self.request.GET.get("min_price"):
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price := self.request.GET.get("max_price"):
+            queryset = queryset.filter(price__lte=max_price)
+        if order_by := self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["total_products"] = self.get_queryset().count()
+        context["categories"] = ProductCategoryModel.objects.all()
         return context
         
 
