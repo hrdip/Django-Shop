b5f5ca3defcfdc52f3e17e0901dfc94aaec8e53a
Hossein L. Raei <hrdip.2018@gmail.com>
2024-04-23 20:57:31 +0800

    similar product
    following same categories

--- core/shop/templatetags/shop_tags.py
+++ core/shop/templatetags/shop_tags.py
@@ -7,3 +7,9 @@ register = template.Library()
 def show_latest_products():
     latest_products = ProductModel.objects.filter(status=ProductStatusType.publish.value).order_by('-created_date')[:8]
     return {"latest_products":latest_products}
+
+@register.inclusion_tag('includes/similar-products.html')
+def show_similar_products(product):
+    product_categories = product.category.all()
+    similar_products = ProductModel.objects.filter(status=ProductStatusType.publish.value,category__in=product_categories).order_by('-created_date')[:4]
+    return {"similar_products":similar_products} 
