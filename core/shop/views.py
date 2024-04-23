94c97666fe5a59f6679c4068c86d6205603c7a66
Hossein L. Raei <hrdip.2018@gmail.com>
2024-04-23 18:03:20 +0800

    reflect our filter when we are selected
    jquery listener (listen to select change) and update or add our changes to url

--- core/shop/views.py
+++ core/shop/views.py
@@ -51,13 +51,44 @@ class ShopProductGridView(ListView):
 
 class ShopProductListView(ListView):
     template_name = 'shop/product-list.html'
-    queryset = ProductModel.objects.filter(status=ProductStatusType.publish.value)
-    paginate_by = 9
+    paginate_by = 9 
+
+    def get_paginate_by(self, queryset):
+        # if page_size are existing return it else return paginate_by
+        return self.request.GET.get('page_size', self.paginate_by)
+
+    # filters and get response
+    def get_queryset(self):
+        queryset = ProductModel.objects.filter(
+            status=ProductStatusType.publish.value)
+        
+        # get query parameters with self.request.GET.get("q") from url
+        # := minimizing this code:  search_q=self.request.GET.get("q")    if search_q:     queryset = queryset.filter(title__icontains=search_q)
+        # := اگر وجود داشت تخصیص بده python 3.8 and up supported
+        if search_q := self.request.GET.get("q"):
+            # we are filtering again that queryset in above
+            # if search_q is existing filter by that (title__icontains=search_q)
+            queryset = queryset.filter(title__icontains=search_q)
+        if category_id := self.request.GET.get("category_id"):
+            queryset = queryset.filter(category__id=category_id)
+        if min_price := self.request.GET.get("min_price"):
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price := self.request.GET.get("max_price"):
+            queryset = queryset.filter(price__lte=max_price)
+        if order_by := self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["total_products"] = self.get_queryset().count()
+        context["categories"] = ProductCategoryModel.objects.all()
         return context
     
+
 class ShopProductDetailView(DetailView):
     template_name = 'shop/product-detail.html'
     queryset = ProductModel.objects.filter(status=ProductStatusType.publish.value)
