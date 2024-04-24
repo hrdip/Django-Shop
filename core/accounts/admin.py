5b698604cbe3596dd22bb7d2a3785e1c7a98228c
Hossein L. Raei <hrdip.2018@gmail.com>
2024-04-24 15:16:45 +0800

    session base shop cart
    save on the server. if using a model base save on the Database. first, we use session after that when the user wants to complete the transaction and needs authentication then we must copy our session on the Database and Handel the transaction

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Profile
 from django.contrib.auth import get_user_model
+from django.contrib.sessions.models import Session
 
 # for dynamically generated
 User = get_user_model( )
@@ -71,6 +72,13 @@ class CustomProfileAdmin(admin.ModelAdmin):
     searching_fields = ("user", "first_name", "last_name", "phone_number")
 
 
+# show unique session data in admin dashboard
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return obj.get_decoded()
+    list_display = ['session_key', '_session_data', 'expire_date']
+    readonly_fields = ['_session_data']
 
 admin.site.register(Profile, CustomProfileAdmin)
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Session, SessionAdmin)
