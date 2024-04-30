a7a317e605d8c815e7b3acf29e4172c9e38dae4f
Hossein L. Raei <hrdip.2018@gmail.com>
2024-04-30 17:46:31 +0800

    delete selected item from summery cart

--- core/cart/urls.py
+++ core/cart/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("session/add-product/", views.SessionAddProductView.as_view(), name="session-add-product"),
     path("session/cart/summery/", views.SessionCartSummeryView.as_view(), name="session-cart-summery"),
     path("session/update-product-quantity/", views.SessionUpdateProductQuantityView.as_view(), name="session-update-product-quantity"),
+    path("session/remove-product/", views.SessionRemoveProductView.as_view(), name="session-remove-product"),
 ]
