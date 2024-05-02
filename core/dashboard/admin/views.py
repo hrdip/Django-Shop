c24d1f5f8caf9bef563ab1fe076b210bbfae2575
Hossein L. Raei <hrdip.2018@gmail.com>
2024-05-02 15:47:12 +0800

    manage permissions

--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -1,11 +1,9 @@
-from django.views.generic import View, TemplateView
+from django.views.generic import  TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
-from django.urls import reverse_lazy
-from accounts.models import UserType
+from dashboard.permissions import HasAdminAccessPermission
 # Create your views here.
 
 
-class AdminDashboardHomeView(LoginRequiredMixin, TemplateView):
+class AdminDashboardHomeView(LoginRequiredMixin,HasAdminAccessPermission, TemplateView):
     template_name = 'dashboard/admin/home.html'
 
